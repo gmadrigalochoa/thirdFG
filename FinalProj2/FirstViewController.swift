dfc6030246fae4d3ca5921d2b4d66899b853b278
Guadalupe Madrigal <gmadrigal@149-14.cpc.local>
2019-04-07 19:51:06 -0700

    Initial Commit

---
+++ FinalProj2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FinalProj2
+//
+//  Created by Guadalupe Madrigal on 4/7/19.
+//  Copyright © 2019 Guadalupe Madrigal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FinalProj2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FinalProj2
+//
+//  Created by Guadalupe Madrigal on 4/7/19.
+//  Copyright © 2019 Guadalupe Madrigal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
